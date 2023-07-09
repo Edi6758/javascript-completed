94475f8451b14a33c78a00be3e0d5889c5f674c2
Edi6758 <ediarts7@gmail.com>
2023-07-09 16:44:58 -0300

    strict mode example

---
+++ strict-mode/index.js
@@ -0,0 +1,12 @@
+/**
+ * use strict forces the user to follow some rules in JS so as not to generate errors
+ */
+"use strict"; //ativate strict mode
+
+//no strict mode
+nome="Bruno";
+console.log(nome);
+
+//strict mode
+let nome="Bruno";
+console.log(nome);
