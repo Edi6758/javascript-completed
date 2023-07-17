089f4c63a1843351b2543142b914ef5c1962eb3e
Edi6758 <ediarts7@gmail.com>
2023-07-17 04:56:40 -0300

    mathematical operators examble

---
+++ mathematical-operators/index.js
@@ -0,0 +1,23 @@
+/**
+ * arithmetic operators are the basic operators for calculating in javascript.
+ */
+
+let num1 = 0, num2 = 0, res = 0;
+
+num1 = 10;
+num2 = 5;
+
+res = num1 + num2;
+console.log(res);
+
+res = num1 - num2;
+console.log(res);
+
+res = num1 * num2;
+console.log(res);
+
+res = num1 / num2;
+console.log(res);
+
+res = num1 % num2;
+console.log(res);
