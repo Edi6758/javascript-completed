4d275f75cf7417b224a523a7fa8eb361125c7c34
Edi6758 <ediarts7@gmail.com>
2023-09-11 00:39:19 -0300

    ternary study

---
+++ ternary/index.js
@@ -0,0 +1,17 @@
+//normal form
+
+let num = 2
+let res = num%2
+
+if(res===0){
+    console.log("par")
+}else{
+    console.log("impar")
+}
+
+//ternary form
+//0 = false
+//1 = true
+let num2 = 2
+res = (!num%2 ? "Par" : "Impar")
+// logic test ? if true : if false
