834e80c2b7a575e68913b196b0c4454b2547e075
Edi6758 <edivaldo.souza@mathex.com.br>
2023-09-25 17:15:41 -0300

    do while study

---
+++ do while/index.js
@@ -0,0 +1,16 @@
+//no do while eu tenho a certeza que o bloco de comando vai ser executado pelo menos uma vez
+
+let n = 10
+while(n<10){
+    console.log("CFB Cursos")
+    n++
+}
+console.log("Fim do programa")
+
+//VS
+
+do{
+    console.log("CFB Cursos")
+    n++
+}while((n<10))
+console.log("Fim do programa")
